9d729362039de39999f39b3d5fbf58cef23b68fe
Murilo Barboza <murilobarboza04@gmail.com>
2024-11-12 19:50:33 -0300

    Project created

---
+++ dslist/src/test/java/com/murilobarboza04/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilobarboza04.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
